865b7220983e60071f880fc8cd375b049ccf6e75
kevin lang <langkevina@gmail.com>
2019-06-09 15:13:10 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
+class CashRegister
 
+  attr_accessor :total, :discount
+  def self.items
+    @items
+  end
+
+  def initialize(amount = nil)
+    @total = 0
+    @discount = 0
+    if amount != nil
+      @discount = amount
+    end
+    @items = []
+  end
+
+  def add_item(name, cost, qty = 1)
+    @total = @total + (cost * qty)
+    i = 0
+    while i < qty
+      i =+ 1
+      @items << name
+    end
+  end
+end
