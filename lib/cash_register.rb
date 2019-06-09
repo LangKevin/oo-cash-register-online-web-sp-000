707a7f3b639e90f42721ff38826ef3aa23d59253
kevin lang <langkevina@gmail.com>
2019-06-09 15:29:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -26,6 +26,7 @@ class CashRegister
   def apply_discount
     if @discount > 0
       @total = @total * (1.0 - (0.01 * @discount))
+      puts "After the discount, the total comes to $+#{@total}."
     end
   end
 
